62e6eb5273be1905e321c9c357b97ef889fcf0f2
gladys1230 <kitchan3495@gmail.com>
2019-12-20 16:11:13 -0600

    add load image function

--- src/NewPetModal.js
+++ src/NewPetModal.js
@@ -1,16 +1,38 @@
-import React, {useState } from 'react';
+import React, {useState, useRef } from 'react';
 import Modal from 'react-modal';
 
 const NewPetModal = ({onCancel}) => {
   const [name, setName] = useState('');
   const [kind, setKind] = useState('');
-  
+  const [photo, setPhoto] = useState(null);
+
+  const photoInput = useRef();
+
+  const updatePhoto = () => {
+    const file =
+    photoInput.current.files &&
+    photoInput.current.files[0];
+
+    if(file) {
+      const reader = new FileReader();
+      reader.onloadend = () => setPhoto(reader.result);
+      reader.readAsDataURL(file);
+    }
+  };
+
   return(
-    <Modal isOpen = {true}
-    onRequestClose={(onCancel)}>
+    <Modal isOpen = {true} onRequestClose={(onCancel)}>
 
       <h2>New Pet</h2>
       <form className="pet-form">
+        {photo && <img alt="the pet" src = {photo} />}
+        <label htmlFor="photo">Photo</label>
+        <input
+          type="file"
+          id="photo"
+          ref={photoInput}
+          onChange={updatePhoto}
+        />
         <label htmlFor="name">Name</label>
         <input
           type="text"
